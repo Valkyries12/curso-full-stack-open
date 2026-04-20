d9242f7b4bfce537b5ebd4d90a123e0b56900859
Nicolás Caruso <nicolas-maximiliano.caruso@capgemini.com>
2026-04-20 15:57:25 -0300

    Se corrige el app del modulo courseInfo

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,8 +1,38 @@
-const App = (props) => {
-  const {counter} = props
+import Content from "./Content";
+import Header from "./Header";
+import Total from "./Total";
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
+  
+
+
+
   return (
-    <div>{counter}</div>
-  )
+    
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
 }
 
 export default App
