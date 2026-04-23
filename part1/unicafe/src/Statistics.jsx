2e110f4ef2bf2fa523c7b1e648c747738ea5a32c
Nicolás Caruso <nico.caruso1992@outlook.com>
2026-04-23 16:29:03 -0300

    Se agrega 1.9: unicafe paso 4

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -5,16 +5,27 @@ const Statistics = ({
     quantity,
     total
 }) => {
+    
     return (
         <>
-            <h2>Statistics</h2>
-            <p>Good: {good}</p>
-            <p>Neutral: {neutral}</p>
-            <p>Bad: {bad}</p>
-            <p>All: {quantity}</p>
-            <p>Average: {total / quantity}</p>
-            <p>Positive: {(good / quantity) * 100}%</p>
+        {
+            quantity == 0 
+            ? <p>No feedback given</p>
+            : (
+            <>
+                <h2>Statistics</h2>
+                <p>Good: {good}</p>
+                <p>Neutral: {neutral}</p>
+                <p>Bad: {bad}</p>
+                <p>All: {quantity}</p>
+                <p>Average: {total / quantity}</p>
+                <p>Positive: {(good / quantity) * 100}%</p>
+            </>
+            )
+        }
         </>
+        
+        
     )
 }
 
