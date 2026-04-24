89a32abe2454a6f9849d7f51dc4694a311128b7c
Nicolás Caruso <nico.caruso1992@outlook.com>
2026-04-24 15:52:22 -0300

    Se agrega 2.9*: La agenda telefonica paso 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: `Arto Hellas ${Math.random()}`, name: 'Arto Hellas', number: "040-1234567" }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]); 
+
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -22,13 +27,26 @@ const App = () => {
     setNewNumber("");
   }
 
+  const filteredPersons = persons.filter((person) => person.name.includes(search));
+
+
+
   return (
     <div>
-      <div>debug: {newName}</div>
       <h2>Phonebook</h2>
+      <div>
+        <label>Filter shown with: </label>
+        <input 
+          type="text" 
+          value={search} 
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      
       <form   
         onSubmit={handleSubmit}
       >
+        <h2>Add a new</h2>
         <div>
           name: <input 
                   value={newName}
@@ -50,7 +68,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {
-        persons.map((person) => <p key={person.id}>{person.name}: {person.number}</p>)
+        filteredPersons.map((person) => <p key={person.id}>{person.name}: {person.number}</p>)
       }
     </div>
   )
