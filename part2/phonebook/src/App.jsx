17646069b8da6dab12340c175fef670d6d58da37
Nicolás Caruso <nico.caruso1992@outlook.com>
2026-04-24 12:43:09 -0300

    Se agrega 2.6: La agenda Telefonica Paso 1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { id: `Arto Hellas ${Math.random()}`, name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      id: `${newName} ${Math.random()}`,
+      name: newName
+    };
+    setPersons([...persons, newPerson]);
+    setNewName("");
+  }
+
+  return (
+    <div>
+      <div>debug: {newName}</div>
+      <h2>Phonebook</h2>
+      <form   
+        onSubmit={handleSubmit}
+      >
+        <div>
+          name: <input 
+                  value={newName}
+                  onChange={(e) => setNewName(e.target.value)}
+                />
+        </div>
+        <div>
+          <button 
+            type="submit"
+          >
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {
+        persons.map((person) => <p key={person.id}>{person.name}</p>)
+      }
+    </div>
+  )
+}
+
+export default App
