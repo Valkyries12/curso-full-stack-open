cab70ae6c75761e515c0b90588b4edf3b110b9b4
Nicolás Caruso <nico.caruso1992@outlook.com>
2026-04-24 09:40:11 -0300

    Se agrega 2.1: Informacion del curso paso 6

--- part1/courseInfo/src/Content.jsx
+++ part1/courseInfo/src/Content.jsx
@@ -6,9 +6,7 @@ const Content = ({ parts }) => {
         {
           parts.map((part) => <Part key={part.name} part={part.name} exercise={part.exercises} />)
         }
-        {/*<Part part={part1.name} exercise={part1.exercises} />
-        <Part part={part2.name} exercise={part2.exercises} />
-        <Part part={part3.name} exercise={part3.exercises} />*/}
+        
       </div>
     );
 }
