3710807d55a20fa28d73052a9abe2c18510006bd
Nicolás Caruso <nico.caruso1992@outlook.com>
2026-04-24 09:43:13 -0300

    Se agrega 2.2: Informacion del curso paso 7
    Se muestra la suma de los ejercicios del curso

--- part1/courseInfo/src/Total.jsx
+++ part1/courseInfo/src/Total.jsx
@@ -2,7 +2,7 @@ const Total = ({parts}) => {
     const exerciseQuantity = parts.reduce((sum, part) => sum + part.exercises, 0);
     
     return (
-        <p>Number of exercises {exerciseQuantity}</p>
+        <p><b>Total of exercises {exerciseQuantity}</b></p>
     )
 }
 
